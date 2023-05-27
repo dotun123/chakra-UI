598fa4c64e7c808ff30d9f4d635bc437ad9b6b6f
Your Name <justiceagunkejoye@gmail.com>
2023-05-27 17:17:03 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Link } from "@chakra-ui/react";
+import Dashboard from "./dashboard";
+
+import React from 'react'
+
+const index = () => {
+  return (
+    <Dashboard/>
+  )
+}
+
+export default index;
